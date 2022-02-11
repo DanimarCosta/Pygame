56bc422570f09d8a5f21fdc90cd92cf7f84ca3d2
Danimar Campos da Costa <danimarcamposcosta@gmail.com>
2022-02-11 16:41:42 -0300

    Menu
    Configuração inicial do menu

--- menu.py
+++ menu.py
@@ -1,1 +1,31 @@
 import pygame
+
+class Menu():
+    def __init__(self, game):
+        self.game = game
+        self. mid_w, self.mid_h = self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2
+        self.run_display = True
+        self.cursor_rect = pygame.rect(0, 0, 20, 20)
+        self.offset = - 100
+
+    def draw_cursor(self):
+        self.game.draw_text('*', 15, self.cursor_rect.x, self.cursor_rect.y)
+
+    def blit_scream(self):
+        self.game.window.blit(self.game.display, (0, 0))
+        pygame.display.update()
+        self.game.reset_keys()
+
+class MainMenu(Menu):
+    def __init__(self, game):
+        Menu.__init__(self, game)
+        self.state = "Start"
+        self.startx, self.starty = self.mid_w, self.mid_h + 30
+        self.optionsx, self.optionsy = self.mid_w, self.mid_h + 50
+        self.creditsx, self.creditsy = self.mid_w, self.mid_h + 70
+        self.cursor_rect.midtop = self.startx + self.offset, self.starty
+
+    def display_menu(self):
+        self.run_display = True
+        while self.run_display:
+            self.game.display.fill(self.game.BLACK)
